1dbb0a6937e1444053f2c898643b2d7b7fdf1817
Korbalu <bdkorosi@gmail.com>
2024-02-26 22:41:46 +0100

    Login/Register OK, Authorization seem to work perfectly...

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
+  constructor(private userService: UserService, private router: Router) {
+  }
+
+  logout() {
+    this.userService.logout().subscribe({
+      next: () => {
+      },
+      error: err => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log("Logout Complete");
+        localStorage.removeItem('token');
+        this.router.navigate(["login"])
+      }
+    })
+  }
+
+  login() {
+    this.router.navigate(["login"])
+  }
+
+  protected readonly localStorage = localStorage;
 }
