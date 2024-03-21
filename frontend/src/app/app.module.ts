bb30dce282c78c77efda9d4b2eaaf5c04775418b
Korbalu <bdkorosi@gmail.com>
2024-03-21 22:11:09 +0100

    app.module.ts ordered

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {LoginComponent} from './components/login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { UserDetailsComponent } from './components/user-details/user-details.component';
-import { CityCreationComponent } from './components/city-creation/city-creation.component';
-import { CityComponent } from './components/city/city.component';
-import { BuilderRecruiterComponent } from './components/builder-recruiter/builder-recruiter.component';
-import { CityListComponent } from './components/city-list/city-list.component';
-import { MarketComponent } from './components/market/market.component';
-import { MissionComponent } from './components/mission/mission.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import {CityCreationComponent} from './components/city-creation/city-creation.component';
+import {CityComponent} from './components/city/city.component';
+import {BuilderRecruiterComponent} from './components/builder-recruiter/builder-recruiter.component';
+import {CityListComponent} from './components/city-list/city-list.component';
+import {MarketComponent} from './components/market/market.component';
+import {MissionComponent} from './components/mission/mission.component';
 
 @NgModule({
   declarations: [
@@ -37,4 +37,5 @@ import { MissionComponent } from './components/mission/mission.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
